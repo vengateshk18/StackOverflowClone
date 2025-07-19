bfe53ea1dabb223ff287d81d18a92bc8947193e3
Vengatesh K <v77206143@gmail.com>
2025-07-19 14:05:44 +0530

    Implemented simple search engine, Introduced guest, Introduced database, and some modifications in old code

--- main.java
+++ main.java
@@ -6,7 +6,15 @@ import java.time.LocalDateTime;
 
 public class main {
     public static void main(String args[]){
-        System.out.println("simple");
+        Search engine=new Search();
+        Member user1=new Member("venky");
+        Question question1=user1.createQuestion(engine, "how to write hellow world in java programming", "I am new to java, exlain me how to write hellow wordld in java", null);
+        Answer ans1=user1.creatAnswer(engine, "Just in main function put this line System.out.println(\"Hellow Word\")", question1, null);
+        Comment qc1=new Comment("Oh just simple thanks", user1, ans1);
+        Guest g=new Guest();
+        for(Question k:g.getAllQuestions(engine)){
+            k.toSting();
+        }
     }
 }
 
@@ -14,22 +22,59 @@ enum Badge{
     BRONZE, SILVER, GOLD , DIAMOND, CROWN, ACE
 }
 
-enum Status{
+enum QuestionStatus{
     OPEN, CLOSED, DELETED
 }
 
-class Member{
-    private int Id;
-    private static int Idcounter=100;
-    private String name;
-    private Enum badge;
-    private List<Question> questions;
-    private int bounty;
-    Member(){
-        this.Id=Idcounter++;
+enum AccountStatus{
+    ACTIVE, ARCHIVED, BLOCKED, BANNED, UNKNOWN
+}
+
+class DataBase{
+    public List<Question> questions;
+    public List<Answer> answers;
+    public List<Comment> comments;
+    public List<Member> members;
+    DataBase(){
+        this.questions=new ArrayList<>();
+        this.answers=new ArrayList<>();
+        this.comments=new ArrayList<>();
+        this.members=new ArrayList<>();
     }
-    public void addBountyToQuestio(int bounty){
-        this.bounty+=bounty;
+    public void addQuestion(Question question){
+        this.questions.add(question);
+    }
+    public void addAswer(Answer answer){
+        this.answers.add(answer);
+    }
+        public void addComment(Comment comment){
+        this.comments.add(comment);
+    }
+    public List<Question> getQuestions(){
+        return this.questions;
+    }
+
+    public List<Answer> getAnswers(){
+        return this.answers;
+    }
+    public List<Comment> gComments(){
+        return this.comments;
+    }
+
+}
+class Member{
+    protected  int Id;
+    protected static int Idcounter=100;
+    protected String name;
+    protected Enum badge;
+    protected Enum status;
+    protected List<Question> questions;
+    Member(String name){
+        this.Id=Idcounter++;
+        this.name=name;
+        this.badge=Badge.BRONZE;
+        this.status=AccountStatus.ACTIVE;
+        this.questions=new ArrayList<>();
     }
     public int getId(){
         return this.Id;
@@ -43,16 +88,37 @@ class Member{
         return this.questions;
     }
 
-    public Question createQuestion(String title, String details,List<Tag> tags){
-        return new Question();
+    public Question createQuestion(DataBase dataBase,String title, String details,List<Tag> tags){
+        Question quObj=new Question(title,details,this);
+        if(tags!=null){
+            for(Tag tag:tags){
+            quObj.addTag(tag);
+        }
+        }
+        dataBase.addQuestion(quObj);
+        return quObj;
     }
 
-    public Answer creatAnswer(String answer, Question question, List<Tag> tags){
-        return new Answer();
+    public Answer creatAnswer(DataBase dataBase,String answer, Question question, List<Tag> tags){
+        Answer ansObj=new Answer(answer,this,question);
+        if(tags!=null){
+            for(Tag tag:tags){
+            ansObj.addTag(tag);
+        }
+        }
+        dataBase.addAswer(ansObj);
+        return ansObj;
     }
 
-    public Comment creaComment(String Comment, Object parent, List<Tag> tags){
-        return new Comment();
+    public Comment createComment(DataBase dataBase,String comment, Object parent, List<Tag> tags){
+        Comment comObj=new Comment(comment,this,parent);
+        if(tags!=null){
+            for(Tag tag:tags){
+            comObj.addTag(tag);
+            }
+        }
+        dataBase.addComment(comObj);
+        return comObj;
     }
 
     public Enum getBatch(){
@@ -61,6 +127,13 @@ class Member{
 
     public Enum setBatch(){
         return this.badge=badge;
+    }
+
+    public Enum getAccountStatus(){
+        return this.status;
+    }
+    public void changeAccountStatus(Enum status){
+        this.status=status;
     }
 }
 
@@ -76,7 +149,16 @@ class Question{
     private Member owner;
     private List<Tag> tags;
 
-    Question(){
+    Question(String title, String details,Member member){
+        this.Id=this.Idcounter++;
+        this.title=title;
+        this.details=details;
+        this.owner=member;
+        this.upvotes=0;
+        this.downvotes=0;
+        this.status=QuestionStatus.OPEN;
+        this.datetime=LocalDateTime.now();
+        this.tags=new ArrayList<>();
 
     }
     public void changeStatus(Enum status){
@@ -114,7 +196,12 @@ class Question{
         this.tags.add(tag);
     }
 
-
+    public void toSting(){
+        System.out.println("Question Id "+this.Id+" Question title "+this.title+" Question details "+this.details);
+        for(Tag tag: this.tags){
+            System.out.println(tag.toString());
+        }
+    }
 
 }
 
@@ -128,8 +215,15 @@ class Answer{
     private Member user;
     private Question question;
     private List<Tag> tags;
-    Answer(){
-
+    Answer(String answer,Member user,Question question){
+        this.Id=Idcounter++;
+        this.answer=answer;
+        this.dateTime=LocalDateTime.now();
+        this.upvotes=0;
+        this.downvotes=0;
+        this.user=user;
+        this.question=question;
+        this.tags=new ArrayList<>();
     }
     public int getId(){
         return this.Id;
@@ -140,7 +234,7 @@ class Answer{
     public Member getUser(){
         return this.user;
     }
-    public Question gQuestion(){
+    public Question getQuestion(){
         return this.question;
     }
     public void upvote(){
@@ -163,6 +257,13 @@ class Answer{
         this.tags.add(tag);
     }
 
+    public void toSting(){
+        System.out.println("Answer Id "+this.Id+" Answer details "+this.answer+" For the question "+this.question.toString());
+        for(Tag tag: this.tags){
+            System.out.println(tag.toString());
+        }
+    }
+
 }
 
 class Comment{
@@ -176,11 +277,17 @@ class Comment{
     private Object parent;
     private List<Tag> tags;
 
-    Comment(){
+    Comment(String comment,Member member,Object parent){
         this.Id=Idcounter++;
         this.upvotes=0;
         this.downvotes=0;
         this.datetime=LocalDateTime.now();
+        this.commentDetails=comment;
+        this.user=member;
+        this.upvotes=0;
+        this.downvotes=0;
+        this.parent=parent;
+        this.tags=new ArrayList<>();
     }
     public int getId(){
         return this.Id;
@@ -224,26 +331,41 @@ class Comment{
     public void addTag(Tag tag){
         this.tags.add(tag);
     }
+
+    public void toSting(){
+        System.out.println("Comment Id "+this.Id+" Comment details "+this.commentDetails+" parent details"+this.parent.toString());
+        for(Tag tag: this.tags){
+            System.out.println(tag.toString());
+        }
+    }
 }
 
-class Moderator{
-    private int Id;
-    private static int Idcounter=100;
-    private String name;
-    private List<Question> deletedQuestions;
-    Moderator(){
-
+class Moderator extends Member{
+    Moderator(String name){
+        super(name);
     }
 
     public void closeQuestion(Question question){
-        question.changeStatus(Status.CLOSED);
+        question.changeStatus(QuestionStatus.CLOSED);
     }
 
     public void reopenQuestion(Question question){
-        question.changeStatus(Status.OPEN);
+        question.changeStatus(QuestionStatus.OPEN);
     }
     public void deletedQuestion(Question question){
-        question.changeStatus(Status.DELETED);
+        question.changeStatus(QuestionStatus.DELETED);
+    }
+}
+
+class Admin extends Member{
+    Admin(String name){
+        super(name);
+    }
+    public void blockMember(Member member){
+        member.changeAccountStatus(AccountStatus.BLOCKED);
+    }
+    public void unblockMember(Member member){
+        member.changeAccountStatus(AccountStatus.ACTIVE);
     }
 }
 
@@ -252,10 +374,100 @@ class Tag{
     private static int Idcounter=100;
     private String details;
 
-    Tag(){
+    Tag(String details){
+        this.details=details;
+    }
 
+    public void toSting(){
+        System.err.println("Tag Id: "+this.Id+" Tag details "+this.details);
     }
 } 
 class TagManager{
-    
+    private HashMap<Tag,Integer> frequency;
+    TagManager(){
+        this.frequency=new HashMap<>();
+    }
+
+    public int getFrequency(Tag tag){
+        return this.frequency.get(tag);
+    }
+
+    public void initializeFrequency(Tag tag){
+        this.frequency.put(tag,0);
+    }
+
+    public void increaseFrequency(Tag tag){
+        this.frequency.put(tag,this.frequency.get(tag)+1);
+    }
+
+
+    public void decreaseFrequency(Tag tag){
+        if(this.frequency.get(tag)>0){
+            this.frequency.put(tag,this.frequency.get(tag)-1);
+        }
+    }
+}
+
+class Search extends DataBase{
+    Search(){
+        super();
+    }
+  public List<Question> searchQuestions(String phrase){
+    String searchStrings[]=phrase.split(" ");
+    List<Question> answer=new ArrayList<>();
+    for(Question question: questions){
+        String completeDetails=question.getTitle()+question.getDetails();
+        int wordMatchingCount=0;
+        for(String word: searchStrings){
+            if(completeDetails.indexOf(word)!=-1){
+                wordMatchingCount+=1;
+            }
+        }
+        if(searchStrings.length/wordMatchingCount>0.35){
+            answer.add(question);
+        }
+    }
+    return answer;
+  }
+  public List<Question> getAllQuestions(){
+    return questions;
+  }
+  public List<Question> deepSearch(String phrase){
+        String searchStrings[]=phrase.split(" ");
+        List<Question> answer=new ArrayList<>();
+        for(Question question: questions){
+            String completeDetails=question.getTitle()+question.getDetails();
+            for(Answer ans:answers){
+                completeDetails+=ans.getAnswer();
+            }
+            for(Comment com:comments){
+                completeDetails+=com.getDetails();
+            }
+            int wordMatchingCount=0;
+            for(String word: searchStrings){
+                if(completeDetails.indexOf(word)!=-1){
+                    wordMatchingCount+=1;
+                }
+            }
+            if(searchStrings.length/wordMatchingCount>0.50){
+                answer.add(question);
+            }
+        }
+        return answer;
+  }
+
+}
+
+class Guest{
+    Guest(){
+
+    }
+
+    public List<Question> getAllQuestions(Search search){
+        return search.getAllQuestions();
+    }
+
+    public List<Question> getQuestions(Search search, String phrase){
+        return search.searchQuestions(phrase);
+    }
 }
